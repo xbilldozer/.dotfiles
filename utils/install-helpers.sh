fbebaccb434fbc2cd531de85783caf60879bc0eb
Bill Alton <balton@hioscar.com>
2025-11-23 16:56:28 -0500

    Add uninstall function; add uninstall to work installer files

--- utils/install-helpers.sh
+++ utils/install-helpers.sh
@@ -35,3 +35,39 @@ stow_install() {
     return 1
   fi
 }
+
+stow_uninstall() {
+  package_dir=$1
+  target_dir=$2
+  packages=$3
+
+  if [[ -z $package_dir ]]; then
+    echo "Must set package_dir as the base directory for the packages you want to uninstall"  
+    return 1
+  fi
+  if [[ -z $target_dir ]]; then
+    echo "Must set target_dir as the directory into which you wish to uninstall your pacakges (usually your home directory)"  
+    return 1
+  fi
+  if [[ -z $packages ]]; then
+    echo "Must provide packages as comma-delimited list of packages to uninstall"  
+    return 1
+  fi
+
+  all_succeeded=true
+  pushd $package_dir
+  for folder in $(echo $packages | sed "s/,/ /g")
+  do
+      if ! stow -t $target_dir -D $folder; then
+        echo "FAILED: un-stowing package $package_dir/$folder" >&2
+        all_succeeded=false
+      fi
+  done
+  popd
+
+  if $all_succeeded; then
+    return 0
+  else
+    return 1
+  fi
+}
